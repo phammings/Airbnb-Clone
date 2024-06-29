89cb7cd83d1c232fffb55634b862cabf184be974
Ryan Pham <ryanlepham9@gmail.com>
2024-06-29 13:23:52 -0400

    Implemented navbar compoenent and styling

--- client/src/app/layout/navbar/navbar.component.ts
+++ client/src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {ButtonModule} from "primeng/button";
+import {ToolbarModule} from "primeng/toolbar";
+import {MenuModule} from "primeng/menu";
+import {MenuItem} from "primeng/api";
+
+import {CategoryComponent} from "./category/category.component";
+import {AvatarComponent} from "./avatar/avatar.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    FontAwesomeModule,
+    ToolbarModule,
+    MenuModule,
+    CategoryComponent,
+    AvatarComponent
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  location = "Anywhere";
+  guests = "Add guests";
+  dates = "Any week";
+
+  // login = () => this.authService.login();
+
+  // logout = () => this.authService.logout();
+
+  currentMenuItems: MenuItem[] | undefined = [];
+
+  ngOnInit(): void {
+    this.fetchMenu();
+  }
+
+  private fetchMenu() {
+    return [
+      {
+        label: "Sign up",
+        styleClass: "font-bold"
+      },
+      {
+        label: "Log in",
+      }
+    ]
+  }
 }
