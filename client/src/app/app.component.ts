cacfb07e33c5307f2dea63390612de1b11b12c2b
Ryan Pham <ryanlepham9@gmail.com>
2024-06-29 14:03:03 -0400

    Fixed bugs removing unimplemented features

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import {ButtonModule} from "primeng/button";
 import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import {NavbarComponent} from "./layout/navbar/navbar.component";
+import {FooterComponent} from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
