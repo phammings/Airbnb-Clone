2b42b9a7db4f9cb5e63a4d2cdc2bf8ccf6603ed0
Ryan Pham <ryanlepham9@gmail.com>
2024-06-29 14:11:17 -0400

    Added Toast service to welcome user

--- client/src/app/layout/navbar/navbar.component.ts
+++ client/src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {ButtonModule} from "primeng/button";
@@ -8,6 +8,7 @@ import {MenuItem} from "primeng/api";
 
 import {CategoryComponent} from "./category/category.component";
 import {AvatarComponent} from "./avatar/avatar.component";
+import { ToastService } from '../toast.service';
 
 @Component({
   selector: 'app-navbar',
@@ -29,6 +30,8 @@ export class NavbarComponent implements OnInit {
   guests = "Add guests";
   dates = "Any week";
 
+  toastService = inject(ToastService);
+
   // login = () => this.authService.login();
 
   // logout = () => this.authService.logout();
@@ -37,6 +40,7 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
     this.fetchMenu();
+    this.toastService.send({severity: "info", summary: "Welcome to the airbnb app"});
   }
 
   private fetchMenu() {
--- client/src/app/layout/toast.service.ts
+++ client/src/app/layout/toast.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable} from "rxjs";
+import {Message} from "primeng/api";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastService {
 
-  constructor() { }
+  INIT_STATE = "INIT";
+
+  private send$ = new BehaviorSubject<Message>({summary: this.INIT_STATE});
+  sendSub = this.send$.asObservable();
+
+  public send(message: Message): void {
+    this.send$.next(message);
+  }
 }
