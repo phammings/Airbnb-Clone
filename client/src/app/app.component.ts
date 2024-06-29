2db74e9538749d514306c7f3f5d8fd72fc05157f
Ryan Pham <ryanlepham9@gmail.com>
2024-06-29 12:58:53 -0400

    Added ngOnInit to init font awesome icons

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import {ButtonModule} from "primeng/button";
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  falconLibrary: FaIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.falconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
