470574456df2798ffb721de06c9feb2af1c87427
Ryan Pham <ryanlepham9@gmail.com>
2024-06-29 13:10:23 -0400

    Implemented avatar compoenent

--- client/src/app/layout/navbar/avatar/avatar.component.ts
+++ client/src/app/layout/navbar/avatar/avatar.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {NgClass} from "@angular/common";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    FontAwesomeModule
+  ],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss'
 })
 export class AvatarComponent {
+
+  imageUrl = input<string>();
+  avatarSize = input<"avatar-sm" | "avatar-xl">();
 
 }
