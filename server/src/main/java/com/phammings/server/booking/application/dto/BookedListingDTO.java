6ab5aa3020dbab60a5a62232ad7bee4900bc9bc9
Ryan Pham <ryanlepham9@gmail.com>
2024-07-06 18:07:19 -0400

    Created BookedListingDTO

---
+++ server/src/main/java/com/phammings/server/booking/application/dto/BookedListingDTO.java
@@ -0,0 +1,17 @@
+package com.phammings.server.booking.application.dto;
+
+import com.phammings.server.listing.application.dto.sub.PictureDTO;
+import com.phammings.server.listing.application.dto.vo.PriceVO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record BookedListingDTO(@Valid PictureDTO cover,
+                               @NotEmpty String location,
+                               @Valid BookedDateDTO dates,
+                               @Valid PriceVO totalPrice,
+                               @NotNull UUID bookingPublicId,
+                               @NotNull UUID listingPublicId) {
+}
