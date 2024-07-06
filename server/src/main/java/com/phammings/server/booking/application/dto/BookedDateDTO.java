708a6794993e3f45d789a3407d312dda9fb6b2cf
Ryan Pham <ryanlepham9@gmail.com>
2024-07-05 22:59:45 -0400

    Updated mapper methods for new booking and checking availiblity

--- server/src/main/java/com/phammings/server/booking/application/dto/BookedDateDTO.java
+++ server/src/main/java/com/phammings/server/booking/application/dto/BookedDateDTO.java
@@ -1,2 +1,11 @@
-package com.phammings.server.booking.application.dto;public class BookedDateDTO {
+package com.phammings.server.booking.application.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.OffsetDateTime;
+
+public record BookedDateDTO(
+        @NotNull OffsetDateTime startDate,
+        @NotNull OffsetDateTime endDate
+) {
 }
--- server/src/main/java/com/phammings/server/booking/application/dto/NewBookingDTO.java
+++ server/src/main/java/com/phammings/server/booking/application/dto/NewBookingDTO.java
@@ -1,2 +1,12 @@
-package com.phammings.server.booking.application.dto;public class NewBookingDTO {
+package com.phammings.server.booking.application.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public record NewBookingDTO(
+        @NotNull OffsetDateTime startDate,
+        @NotNull OffsetDateTime endDate,
+        @NotNull UUID listingPublicId) {
 }
