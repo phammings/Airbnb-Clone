82349320edbbc72e7e2ea3c94001f8e3758b5eb3
Ryan Pham <ryanlepham9@gmail.com>
2024-07-05 21:49:58 -0400

    Updated ListerMapper to map info for display page

--- server/src/main/java/com/phammings/server/listing/mapper/ListingMapper.java
+++ server/src/main/java/com/phammings/server/listing/mapper/ListingMapper.java
@@ -3,6 +3,7 @@ package com.phammings.server.listing.mapper;
 import com.phammings.server.listing.application.dto.CreatedListingDTO;
 import com.phammings.server.listing.application.dto.DisplayCardListingDTO;
 import com.phammings.server.listing.application.dto.SaveListingDTO;
+import com.phammings.server.listing.application.dto.DisplayListingDTO;
 import com.phammings.server.listing.application.dto.vo.PriceVO;
 import com.phammings.server.listing.domain.Listing;
 import org.mapstruct.Mapper;
@@ -40,4 +41,15 @@ public interface ListingMapper {
     default PriceVO mapPriceToPriceVO(int price){
         return new PriceVO(price);
     }
+
+    @Mapping(target = "landlord", ignore = true)
+    @Mapping(target = "description.title.value", source = "title")
+    @Mapping(target = "description.description.value", source = "description")
+    @Mapping(target = "infos.bedrooms.value", source = "bedrooms")
+    @Mapping(target = "infos.guests.value", source = "guests")
+    @Mapping(target = "infos.beds.value", source = "beds")
+    @Mapping(target = "infos.baths.value", source = "bathrooms")
+    @Mapping(target = "category", source = "bookingCategory")
+    @Mapping(target = "price.value", source = "price")
+    DisplayListingDTO listingToDisplayListingDTO(Listing listing);
 }
