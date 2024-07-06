74a8eeec879502a95079609d0dc1f4a6c4a36038
Ryan Pham <ryanlepham9@gmail.com>
2024-07-06 18:07:37 -0400

    Created BookingService to display user's bookings

--- server/src/main/java/com/phammings/server/booking/application/BookingService.java
+++ server/src/main/java/com/phammings/server/booking/application/BookingService.java
@@ -1,12 +1,15 @@
 package com.phammings.server.booking.application;
 
 import com.phammings.server.booking.application.dto.BookedDateDTO;
+import com.phammings.server.booking.application.dto.BookedListingDTO;
 import com.phammings.server.booking.application.dto.NewBookingDTO;
 import com.phammings.server.booking.domain.Booking;
 import com.phammings.server.booking.mapper.BookingMapper;
 import com.phammings.server.booking.repository.BookingRepository;
 import com.phammings.server.listing.application.LandlordService;
+import com.phammings.server.listing.application.dto.DisplayCardListingDTO;
 import com.phammings.server.listing.application.dto.ListingCreateBookingDTO;
+import com.phammings.server.listing.application.dto.vo.PriceVO;
 import com.phammings.server.sharedkernel.service.State;
 import com.phammings.server.user.application.UserService;
 import com.phammings.server.user.application.dto.ReadUserDTO;
@@ -70,5 +73,40 @@ public class BookingService {
     public List<BookedDateDTO> checkAvailability(UUID publicId) {
         return bookingRepository.findAllByFkListing(publicId)
                 .stream().map(bookingMapper::bookingToCheckAvailability).toList();
+    }
+    @Transactional(readOnly = true)
+    public List<BookedListingDTO> getBookedListing() {
+        ReadUserDTO connectedUser = userService.getAuthenticatedUserFromSecurityContext();
+        List<Booking> allBookings = bookingRepository.findAllByFkTenant(connectedUser.publicId());
+        List<UUID> allListingPublicIDs = allBookings.stream().map(Booking::getFkListing).toList();
+        List<DisplayCardListingDTO> allListings = landlordService.getCardDisplayByListingPublicId(allListingPublicIDs);
+        return mapBookingToBookedListing(allBookings, allListings);
+    }
+
+    private List<BookedListingDTO> mapBookingToBookedListing(List<Booking> allBookings, List<DisplayCardListingDTO> allListings) {
+        return allBookings.stream().map(booking -> {
+            DisplayCardListingDTO displayCardListingDTO = allListings
+                    .stream()
+                    .filter(listing -> listing.publicId().equals(booking.getFkListing()))
+                    .findFirst()
+                    .orElseThrow();
+            BookedDateDTO dates = bookingMapper.bookingToCheckAvailability(booking);
+            return new BookedListingDTO(displayCardListingDTO.cover(),
+                    displayCardListingDTO.location(),
+                    dates, new PriceVO(booking.getTotalPrice()),
+                    booking.getPublicId(), displayCardListingDTO.publicId());
+        }).toList();
+    }
+
+    @Transactional
+    public State<UUID, String> cancel(UUID bookingPublicId, UUID listingPublicId) {
+        ReadUserDTO connectedUser = userService.getAuthenticatedUserFromSecurityContext();
+        int deleteSuccess = bookingRepository.deleteBookingByFkTenantAndPublicId(connectedUser.publicId(), bookingPublicId);
+
+        if(deleteSuccess >= 1) {
+            return State.<UUID, String>builder().forSuccess(bookingPublicId);
+        } else {
+            return State.<UUID, String>builder().forError("Booking not found");
+        }
     }
 }
