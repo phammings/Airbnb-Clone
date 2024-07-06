163d4ef814bf474e56cf19627e5c57e2cc3c8c37
Ryan Pham <ryanlepham9@gmail.com>
2024-07-05 21:52:01 -0400

    Created mapping /get-one to display a singlar page

--- server/src/main/java/com/phammings/server/listing/presentation/TenantResource.java
+++ server/src/main/java/com/phammings/server/listing/presentation/TenantResource.java
@@ -2,14 +2,21 @@ package com.phammings.server.listing.presentation;
 
 import com.phammings.server.listing.application.TenantService;
 import com.phammings.server.listing.application.dto.DisplayCardListingDTO;
+import com.phammings.server.listing.application.dto.DisplayListingDTO;
 import com.phammings.server.listing.domain.BookingCategory;
+import com.phammings.server.sharedkernel.service.State;
+import com.phammings.server.sharedkernel.service.StatusNotification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/tenant-listing")
@@ -26,5 +33,16 @@ public class TenantResource {
     public ResponseEntity<Page<DisplayCardListingDTO>> findAllByBookingCategory(Pageable pageable,
                                                                                 @RequestParam BookingCategory category) {
         return ResponseEntity.ok(tenantService.getAllByCategory(pageable, category));
+    }
+
+    @GetMapping("/get-one")
+    public ResponseEntity<DisplayListingDTO> getOne(@RequestParam UUID publicId) {
+        State<DisplayListingDTO, String> displayListingState = tenantService.getOne(publicId);
+        if(displayListingState.getStatus().equals(StatusNotification.OK)) {
+            return ResponseEntity.ok(displayListingState.getValue());
+        } else {
+            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, displayListingState.getError());
+            return ResponseEntity.of(problemDetail).build();
+        }
     }
 }
