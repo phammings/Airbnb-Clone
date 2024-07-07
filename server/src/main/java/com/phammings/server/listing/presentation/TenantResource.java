829233cc334c648585a35343c5a943ec232ff198
Ryan Pham <ryanlepham9@gmail.com>
2024-07-06 20:30:46 -0400

    Added Post mapping for search functionality

--- server/src/main/java/com/phammings/server/listing/presentation/TenantResource.java
+++ server/src/main/java/com/phammings/server/listing/presentation/TenantResource.java
@@ -2,19 +2,18 @@ package com.phammings.server.listing.presentation;
 
 import com.phammings.server.listing.application.TenantService;
 import com.phammings.server.listing.application.dto.DisplayCardListingDTO;
+import com.phammings.server.listing.application.dto.SearchDTO;
 import com.phammings.server.listing.application.dto.DisplayListingDTO;
 import com.phammings.server.listing.domain.BookingCategory;
 import com.phammings.server.sharedkernel.service.State;
 import com.phammings.server.sharedkernel.service.StatusNotification;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -44,5 +43,10 @@ public class TenantResource {
             ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, displayListingState.getError());
             return ResponseEntity.of(problemDetail).build();
         }
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<Page<DisplayCardListingDTO>> search(Pageable pageable, @Valid @RequestBody SearchDTO searchDTO) {
+        return ResponseEntity.ok(tenantService.search(pageable, searchDTO));
     }
 }
