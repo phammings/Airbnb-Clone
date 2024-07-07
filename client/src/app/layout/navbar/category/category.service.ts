c1080340e0d29898135a82877426c4dd1a06e023
Ryan Pham <ryanlepham9@gmail.com>
2024-07-06 20:39:27 -0400

    Removed unused import

--- client/src/app/layout/navbar/category/category.service.ts
+++ client/src/app/layout/navbar/category/category.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Category, CategoryName} from "./category.model";
-import {BehaviorSubject, Observable} from "rxjs";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
