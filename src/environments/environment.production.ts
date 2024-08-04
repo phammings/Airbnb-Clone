23e1017bc5fe1888a599a5e6a44bb5fb2fc897e0
Ryan Pham <ryanlepham9@gmail.com>
2024-08-04 19:15:10 -0400

    Updated production auth0 client id

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -3,6 +3,6 @@ const BACKEND_URL = "https://airbnb-backend.ryanlepham.com";
 export const environment = {
   BACKEND_URL: BACKEND_URL,
   API_URL: `${BACKEND_URL}/api`,
-  AUTH0_CLIENT_ID: "DHds0cBHcyDjN84FcouznygCT17ohczG",
+  AUTH0_CLIENT_ID: "fDIjF2oBzeg5qhhI4lgv1LIqgrllVrBH",
   AUTH0_DOMAIN: "https://dev-xqrv0tv2lnvtpqh1.ca.auth0.com",
 };
